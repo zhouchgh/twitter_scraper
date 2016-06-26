4cf679c1118e086fe62fb9978445b0336bb670de
zhouchgh <zhouch12345@gmail.com>
2016-06-26 13:14:46 -0700

    add user information scraper

--- database.sql
+++ database.sql
@@ -1,4 +1,29 @@
 -------------------------------------------------------
+--       create user table
+-------------------------------------------------------
+create table if not exists `user`
+(
+	Id bigint(20) not null auto_increment,
+	CreateTime datetime not null,
+  	UserCreateTime datetime not null,
+  	UserId varchar(128) collate utf8mb4_unicode_ci not null,
+	ScreenName varchar(128) collate utf8mb4_unicode_ci not null,
+	Name varchar(128) collate utf8mb4_unicode_ci null,
+  	Description varchar(2048) collate utf8mb4_unicode_ci null,
+  	Url varchar(256) collate utf8mb4_unicode_ci not null,
+	Location varchar(256) collate utf8mb4_unicode_ci null,
+	FollowersCount int not null,
+	FollowingCount int not null,
+	FriendsCount int not null,
+	FavouritesCount int not null,
+	ListedCount int not null,
+	StatusesCount int not null,
+	Verified bit not null,
+  	primary key (Id),
+  	unique key UserId (UserId)
+) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_ci;
+
+-------------------------------------------------------
 --       create tweet table
 -------------------------------------------------------
 create table if not exists tweet
@@ -51,5 +76,81 @@ on duplicate key update
 	UserId = values(UserId),
 	Content = values(Content),
 	Url = values(Url);
+end//
+delimiter ;
+
+-------------------------------------------------------
+--      create merge_user store procedure
+-------------------------------------------------------
+delimiter //
+create procedure merge_user
+(
+	in CreateTime datetime,
+  	in UserCreateTime datetime,
+  	in UserId varchar(128) character set utf8mb4,
+	in ScreenName varchar(128) character set utf8mb4,
+	in `Name` varchar(128) character set utf8mb4,
+  	in Description varchar(2048) character set utf8mb4,
+  	in Url varchar(256) character set utf8mb4,
+	in Location varchar(256) character set utf8mb4,
+	in FollowersCount int,
+	in FollowingCount int,
+	in FriendsCount int,
+	in FavouritesCount int,
+	in ListedCount int,
+	in StatusesCount int,
+	in Verified bit
+)
+begin
+insert into `user`
+(
+	CreateTime,
+  	UserCreateTime,
+  	UserId,
+	ScreenName,
+	`Name`,
+  	Description,
+  	Url,
+	Location,
+	FollowersCount,
+	FollowingCount,
+	FriendsCount,
+	FavouritesCount,
+	ListedCount,
+	StatusesCount,
+	Verified
+)
+values
+(
+	CreateTime,
+  	UserCreateTime,
+  	UserId,
+	ScreenName,
+	`Name`,
+  	Description,
+  	Url,
+	Location,
+	FollowersCount,
+	FollowingCount,
+	FriendsCount,
+	FavouritesCount,
+	ListedCount,
+	StatusesCount,
+	Verified
+)
+on duplicate key update
+  	UserCreateTime = values(UserCreateTime),
+	ScreenName = values(ScreenName),
+	`Name` = values(`Name`),
+  	Description = values(Description),
+  	Url = values(Url),
+	Location = values(Location),
+	FollowersCount = values(FollowersCount),
+	FollowingCount = values(FollowingCount),
+	FriendsCount = values(FriendsCount),
+	FavouritesCount = values(FavouritesCount),
+	ListedCount = values(ListedCount),
+	StatusesCount = values(StatusesCount),
+	Verified = values(Verified);
 end//
 delimiter ;
--- twitter_scraper.py
+++ twitter_scraper.py
@@ -16,7 +16,7 @@ execfile("twitter_scraper.conf", config)
 #-----------------------------------------------------------------------
 # create twitter API object
 #-----------------------------------------------------------------------
-api = twitter.Api(config['consumer_key'], config['consumer_secret'], config['access_token_key'], config['access_token_secret'])
+api = twitter.Api(config['consumer_key'], config['consumer_secret'], config['access_token_key'], config['access_token_secret'], sleep_on_rate_limit=True)
 
 initial_schedule_time = datetime.now()
 
@@ -37,7 +37,11 @@ results = api.GetSearch(raw_query = config['twitter_search_query'])
 # Loop through each of the results, and print its content.
 #-----------------------------------------------------------------------
 
+twitter_user_profile_template = config['twitter_user_profile_template']
+twitter_url_template = config['twitter_url_template']
+
 for result in results:
+	# insert tweet information
 	current_time = datetime.now()
 	tweet_create_time = datetime.strptime(result.created_at,'%a %b %d %H:%M:%S +0000 %Y').strftime('%Y-%m-%d %H:%M:%S')
 	tweet_id = result.id_str
@@ -45,6 +49,21 @@ for result in results:
 	content = result.text.encode('ascii', 'ignore')
 	url = twitter_url_template.replace('{UserId}', user_id).replace('{TweetId}', tweet_id)
 	cursor.callproc('merge_tweet', (current_time, tweet_create_time, tweet_id, user_id, content, url,))
+	# insert user information
+	user_create_time = datetime.strptime(result.user.created_at,'%a %b %d %H:%M:%S +0000 %Y').strftime('%Y-%m-%d %H:%M:%S')
+	user_screen_name = result.user.screen_name
+	user_name = result.user.name
+	user_description = result.user.description
+	user_url = twitter_user_profile_template.replace('{UserScreenName}', user_screen_name)
+	user_location = result.user.location
+	user_followers_count = result.user.followers_count
+	user_followings_count = result.user.friends_count
+	user_friends_count = result.user.friends_count
+	user_favourites_count = result.user.favourites_count
+	user_listed_count = result.user.listed_count
+	user_status_count = result.user.statuses_count
+	user_verified = result.user.verified
+	cursor.callproc('merge_user', (current_time, user_create_time, user_id, user_screen_name, user_name, user_description, user_url, user_location, user_followers_count, user_followings_count, user_friends_count, user_favourites_count, user_listed_count, user_status_count, user_verified,))
 conn.commit()
 conn.close()
 
